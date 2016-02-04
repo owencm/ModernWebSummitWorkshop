e54be8fa3de5e1c9987a8dc88cf2b0be1b66dbf9
Owen Campbell-Moore <owencmoore@gmail.com>
2016-02-04 17:26:54 +0530

    Add GCM sender ID and API key

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8000;
 
+// The GCM API key is AIzaSyDNlm9R_w_0FDGjSM1fzyx5I5JnJBXACqU
+
 app.use(express.static(__dirname));
 
 app.listen(PORT, function() {
