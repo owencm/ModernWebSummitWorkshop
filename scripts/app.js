4a8bbe6d231e28c07fe04cd6710152de5b30402b
Alex Russell <slightlyoff@chromium.org>
2016-02-04 16:42:36 +0530

    More URL fixes; enables Service Worker.

--- scripts/app.js
+++ scripts/app.js
@@ -233,7 +233,7 @@
   // Add feature check for Service Workers here
   if('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('/service-worker.js')
+             .register('./service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 
