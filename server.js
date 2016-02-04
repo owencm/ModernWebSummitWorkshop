e483f8d3fcdca83124f8314acac562b230fd4e5e
Owen Campbell-Moore <owencmoore@gmail.com>
2016-02-04 18:12:03 +0530

    Push subscriptions now work

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 var PORT = process.env.PORT || 8000;
 var gcm = require('node-gcm');
 
@@ -7,31 +9,38 @@ var gcm = require('node-gcm');
 
 app.use(express.static(__dirname));
 
-app.use("/push/:token", function(req, res, next) {
-    var senderToken=req.params["token"];
+app.use(bodyParser.json());
+
+app.use("/push", function(req, res, next) {
+  if (req.body.action === 'subscribe') {
+    var subEndpoint = req.body.subscription.endpoint;
+    var senderToken = subEndpoint.slice(40, subEndpoint.length);
     if(!senderToken){
-      res.status(500).set('Content-Type', 'text/plain').send({text:"No sender token found!!!",status:"500"});
+      res.status(500)
+         .send({text:"No sender token found!!!",status:"500"});
     }
 
     var message = new gcm.Message({});
     var sender = new gcm.Sender('AIzaSyDNlm9R_w_0FDGjSM1fzyx5I5JnJBXACqU');
     var registrationTokens = [];
-    registrationTokens.push(req.params["token"]);
+    registrationTokens.push(senderToken);
     setTimeout(function() {
-        console.log("[[SENDING PUSH NOW: "+registrationTokens+"]]");
-        sender.send(message, {
-
-            registrationTokens: registrationTokens
-        }, function(err, response) {
-            if (err)
-                console.error("PUSH ERR: " + err);
-            else
-                console.log("PUSH RESPONSE: " + response);
-        });
-    }, 5000);
-    res.set('Content-Type', 'text/plain').send({text:'Sending push in 5',status:"200"});
+      console.log("[[SENDING PUSH NOW: "+registrationTokens+"]]");
+      sender.send(message, {
+        registrationTokens: registrationTokens
+      }, function(err, response) {
+        if (err)
+          console.error("PUSH ERR: " + err);
+        else
+          console.log("PUSH RESPONSE: ", response);
+      });
+    }, 0);
+    res.send({text:'Sending push in 5',status:"200"});
+  } else {
+    throw new Error ('Unsupported action');
+  }
 })
 
 app.listen(PORT, function() {
-    console.log('express server listening on port ', PORT);
+  console.log('express server listening on port ', PORT);
 });
