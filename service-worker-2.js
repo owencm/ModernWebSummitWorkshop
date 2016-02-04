e509b347e5414753914303e296c9c9804f7cc92f
Boopathi Rajaa <me@boopathi.in>
2016-02-04 17:26:50 +0530

    Convert to toolbox strategies

--- service-worker-2.js
+++ service-worker-2.js
@@ -21,4 +21,37 @@ var filesToCache = [
   './images/wind.png'
 ];
 
-console.log(toolbox);
+toolbox.options.cache.name = cacheName;
+toolbox.precache(filesToCache);
+
+// activate event
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        console.log('[ServiceWorker] Removing old cache', key);
+        if (key !== cacheName && key.indexOf("$$$inactive$$$") === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+var nosw = 0;
+self.addEventListener('fetch', function(e) {
+  var url = new URL(e.request.url);
+  if (nosw || (url.search.indexOf("nosw=1") >= 0)) {
+    nosw = 1;
+    return;
+  }
+});
+
+toolbox.router.get('/(.*)', toolbox.networkFirst, {
+  domain: 'https://publicdata-weather.firebaseio.com',
+  cache: { name: dataCacheName }
+});
+toolbox.router.get('/(.*)', toolbox.cacheFirst, {
+  cache: { name: cacheName }
+});
