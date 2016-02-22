03c9b69836dcf5d049046a1b50f13379bd6db514
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-02-22 11:58:17 +0000

    Add support for the standard Web Push protocol

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var app = express();
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var PORT = process.env.PORT || 8000;
-var gcm = require('node-gcm');
+var webPush = require('web-push');
 
 // The GCM API key is AIzaSyDNlm9R_w_0FDGjSM1fzyx5I5JnJBXACqU
+webPush.setGCMAPIKey('AIzaSyDNlm9R_w_0FDGjSM1fzyx5I5JnJBXACqU');
 
 app.use(express.static(__dirname));
 
@@ -13,26 +14,21 @@ app.use(bodyParser.json());
 
 app.use("/push", function(req, res, next) {
   if (req.body.action === 'subscribe') {
-    var subEndpoint = req.body.subscription.endpoint;
-    var senderToken = subEndpoint.slice(40, subEndpoint.length);
-    if(!senderToken){
-      res.status(500)
-         .send({text:"No sender token found!!!",status:"500"});
-    }
+    var endpoint = req.body.subscription.endpoint;
 
-    var message = new gcm.Message({});
-    var sender = new gcm.Sender('AIzaSyDNlm9R_w_0FDGjSM1fzyx5I5JnJBXACqU');
-    var registrationTokens = [];
-    registrationTokens.push(senderToken);
     setTimeout(function() {
-      console.log("[[SENDING PUSH NOW: "+registrationTokens+"]]");
-      sender.send(message, {
-        registrationTokens: registrationTokens
-      }, function(err, response) {
-        if (err)
-          console.error("PUSH ERR: " + err);
-        else
+      console.log("[[SENDING PUSH NOW: " + endpoint + "]]");
+
+      webPush.sendNotification(endpoint)
+      .then(function(response) {
+        if (response) {
           console.log("PUSH RESPONSE: ", response);
+        } else {
+          console.log("PUSH SENT");
+        }
+      })
+      .catch(function(err) {
+        console.error("PUSH ERR: " + err);
       });
     }, 5000);
     res.send({text:'Sending push in 5',status:"200"});
