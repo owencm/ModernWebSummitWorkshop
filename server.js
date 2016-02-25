3fcf63902a2b154b8c24902207ed510ccf083b70
Owen Campbell-Moore <owencmoore@gmail.com>
2016-02-24 18:00:00 -0800

    Send a notification immediately after subscribing

--- server.js
+++ server.js
@@ -16,20 +16,11 @@ app.use("/push", function(req, res, next) {
   if (req.body.action === 'subscribe') {
     var endpoint = req.body.subscription.endpoint;
 
+    // Send a push once immediately after subscribing, and one 5 seconds later
+    sendNotification(endpoint);
     setTimeout(function() {
       console.log("[[SENDING PUSH NOW: " + endpoint + "]]");
-
-      webPush.sendNotification(endpoint)
-      .then(function(response) {
-        if (response) {
-          console.log("PUSH RESPONSE: ", response);
-        } else {
-          console.log("PUSH SENT");
-        }
-      })
-      .catch(function(err) {
-        console.error("PUSH ERR: " + err);
-      });
+      sendNotification(endpoint);
     }, 5000);
     res.send({text:'Sending push in 5',status:"200"});
   } else {
@@ -46,3 +37,18 @@ app.use("/pushdata",function(req,res,next){
 app.listen(PORT, 'localhost', function() {
   console.log('express server listening on port', PORT);
 });
+
+function sendNotification(endpoint) {
+  console.log('endpoint', endpoint)
+  webPush.sendNotification(endpoint)
+  .then(function(response) {
+    if (response) {
+      console.log("PUSH RESPONSE: ", response);
+    } else {
+      console.log("PUSH SENT");
+    }
+  })
+  .catch(function(err) {
+    console.error("PUSH ERR: " + err);
+  });
+}
